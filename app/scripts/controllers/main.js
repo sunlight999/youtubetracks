c093a002f46e4a907389187702866966e38ca052
jlvigour <jean-louisvigouroux@jlvigours-computer.local>
2015-10-27 19:39:54 +0100

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name youtubetracksApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the youtubetracksApp
+ */
+angular.module('youtubetracksApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
